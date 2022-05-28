61a585bbe1657fe449de6956a721e59dc237c4c7
VikkiT <105386251+VikkiT@users.noreply.github.com>
2022-05-28 20:34:46 +0200

    Update hello .c

--- hello .c
+++ hello .c
@@ -1,1 +1,25 @@
+#include <cs50.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
+
+get_string 
+= string answer ("What's your name\n");
+
+int main (void);
+
+
+#include <cs50.h>
+  
+{   
+     printf ("%s", answer);
+
+get_string = string answer ("What's your name?\n");
+
+printf ("hello, "%s", name");
+
+}
+
 
